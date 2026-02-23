d566bd8577a56ddb940b71c5664cc6001b4c4be5
Neelesh-jatav <neeleshkumar22j@gmail.com>
2026-02-24 02:22:33 +0530

    fix: add Set-Cookie to CORS exposedHeaders so browser accepts cookie from backend

--- server/server.js
+++ server/server.js
@@ -43,6 +43,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Set-Cookie'], // Allow browser to see Set-Cookie header
 }));
 
 // Handle URL-encoded data
